9fc9ca12697e17d0fde21a325bd72bd6839135a1
Hyago Gabriel <hyago.gabriel2017@gmail.com>
2025-05-26 09:59:16 -0300

    fix: build desenvolvimento

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,6 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideNgxMask } from 'ngx-mask';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter([]), 
-    provideNgxMask()
-  ]
-};
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
